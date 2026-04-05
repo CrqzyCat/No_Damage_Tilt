112f05ca2a6bc6ec41bae8a4b803e963e2202d6e
crqzycat <crqzycat@gmail.com>
2026-04-05 14:23:20 +0200

    Initial commit

---
+++ src/client/java/no_damage_tilt/NoDamageTiltClient.java
@@ -0,0 +1,10 @@
+package no_damage_tilt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoDamageTiltClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("No Damage Tilt geladen!");
+    }
+}
---
+++ src/client/java/no_damage_tilt/mixin/DamageTiltMixin.java
@@ -0,0 +1,17 @@
+package no_damage_tilt.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class DamageTiltMixin {
+
+    @Inject(method = "tiltViewWhenHurt", at = @At("HEAD"), cancellable = true)
+    private void stopDamageTilt(MatrixStack matrices, float tickDelta, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
