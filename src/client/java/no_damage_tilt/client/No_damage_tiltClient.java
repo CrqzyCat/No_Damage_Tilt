2a8e3ac8c8b320a592f4064370c0e1989ba3b66e
crqzycat <crqzycat@gmail.com>
2026-04-05 14:20:50 +0200

    Initial commit

---
+++ src/client/java/no_damage_tilt/client/No_damage_tiltClient.java
@@ -0,0 +1,10 @@
+package no_damage_tilt.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class No_damage_tiltClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/no_damage_tilt/No_damage_tilt.java
@@ -0,0 +1,10 @@
+package no_damage_tilt;
+
+import net.fabricmc.api.ModInitializer;
+
+public class No_damage_tilt implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
